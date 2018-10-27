b4567841992246ac2a018add8b9067bc278d60f4
merubador <32394627+merubador@users.noreply.github.com>
2018-10-27 14:27:05 +0300

    Edit selectors.js

--- src/selectors.js
+++ src/selectors.js
@@ -5,8 +5,8 @@ export const getBoardMap = state => state.board;
 export const getSelected = state => state.selected;
 
 export const getDisabledTiles = R.pipe(
-  getBoardMap,
-  R.map(item => !!item.isDisabled && true)
+  state => getBoardMap(state),
+  data => R.map(item => !!item.isDisabled && true)(data)
 );
 
 export const getBoard = state => {
